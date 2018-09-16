5f33a7c4213d267b0c4e7a68f8a3bcb459c3efc5
Psychotheus <psychotheus@gmail.com>
2018-09-16 23:52:36 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,87 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+
+    this.priority = {
+      '+': 1,
+      '-': 1,
+      '*': 2,
+      '/': 2,
+      '^': 3
+    };
+
+    this.ops = [];
+    this.ops.push({operand: '', value: initialValue});
   }
 
   add(number) {
-    // your implementation
+    this.ops.push({operand: '+', value: number});
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.ops.push({operand: '-', value: number});
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.ops.push({operand: '*', value: number});
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.ops.push({operand: '/', value: number});
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.ops.push({operand: '^', value: number});
+    return this;
+  }
+  
+  operate(i) {
+    switch(this.ops[i].operand) {
+      case '+': 
+        if (this.ops[i - 1].operand == '-') {
+          this.ops[i - 1].value -= this.ops[i].value;
+        } else {
+          this.ops[i - 1].value += this.ops[i].value;
+        }
+        break;
+      case '-':
+        if (this.ops[i - 1].operand == '-') {
+          this.ops[i - 1].value += this.ops[i].value;
+        } else {
+          this.ops[i - 1].value -= this.ops[i].value;
+        }
+        break;
+      case '*':
+        this.ops[i - 1].value *= this.ops[i].value;
+        break;
+      case '/':
+        this.ops[i - 1].value /= this.ops[i].value;
+        break;
+      case '^': 
+        this.ops[i - 1].value = Math.pow(this.ops[i - 1].value, this.ops[i].value);
+        break;
+    }
+  }
+
+  valueOf() {
+    this.ops[0].operand = '+';
+    let i = this.ops.length - 1;
+
+    while (i != 0) {
+      if (this.priority[this.ops[i].operand] >= 
+        this.priority[this.ops[i - 1].operand]) {
+          this.operate(i);
+          this.ops.splice(i, 1);
+          i = this.ops.length - 1;
+      } else {
+        i--;
+      }
+    }
+
+    return this.ops[0].value;
   }
 }
 
